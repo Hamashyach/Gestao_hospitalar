0d9caa525a4a3b8fd1ea61d03e3c632a8ca3bba9
Steh_Hamashyach <137620415+Hamashyach@users.noreply.github.com>
2024-12-09 09:24:37 -0300

    Update Pessoa.java

--- Gestao_Hospitalar/src/gestao_hospitalar/Pessoa.java
+++ Gestao_Hospitalar/src/gestao_hospitalar/Pessoa.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package gestao_hospitalar;
 
-/**
- *
- * @author aluno
- */
 public class Pessoa {
     protected String nome;
     protected String identidade;
